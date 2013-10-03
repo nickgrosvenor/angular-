017c398c32e60d264be773df8aa37e2cf6cf6323
nickgrosvenor <rooski@mac.com>
2013-10-03 14:25:12 -0700

    angular base start

---
+++ ticTacCtrl.js
@@ -0,0 +1,20 @@
+function TicTacCtrl($scope){
+	
+	$scope.ticTacToe=[['X','',''],['','',''],['','','']];
+
+	var playerTurn = 1;
+
+	
+	$scope.clickSquare = function(row,column){
+			
+		if(playerTurn % 2 == 1)
+
+			event.target.innnerHTML = "X";
+		else
+			event.target.innerHTML = "O";
+
+		++playerTurn;
+	
+	};
+
+}
